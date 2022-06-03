24b6f5a7e6ca4e3297737aae9cfbaa036bae5904
Claudio-Abuloc-III <abulociiiclaudio@gmail.com>
2022-06-03 21:29:20 +0800

    Update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tic_Tac_Toe_Application
 {
-    partial class Form1
+    partial class TicTacToe
     {
         /// <summary>
         /// Required designer variable.
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.Box = new System.Windows.Forms.GroupBox();
-            this.UpLeftButton = new System.Windows.Forms.Button();
-            this.UpButton = new System.Windows.Forms.Button();
-            this.UpRightButton = new System.Windows.Forms.Button();
-            this.LeftButton = new System.Windows.Forms.Button();
-            this.MidButton = new System.Windows.Forms.Button();
-            this.RightButton = new System.Windows.Forms.Button();
-            this.DownLeftButton = new System.Windows.Forms.Button();
-            this.DownButton = new System.Windows.Forms.Button();
-            this.DownRightButton = new System.Windows.Forms.Button();
             this.ResetButton = new System.Windows.Forms.Button();
+            this.DownRightButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.DownLeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.MidButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.UpRightButton = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.UpLeftButton = new System.Windows.Forms.Button();
             this.Box.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,96 +61,6 @@
             this.Box.TabIndex = 0;
             this.Box.TabStop = false;
             // 
-            // UpLeftButton
-            // 
-            this.UpLeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpLeftButton.Location = new System.Drawing.Point(34, 137);
-            this.UpLeftButton.Name = "UpLeftButton";
-            this.UpLeftButton.Size = new System.Drawing.Size(166, 125);
-            this.UpLeftButton.TabIndex = 0;
-            this.UpLeftButton.UseVisualStyleBackColor = true;
-            this.UpLeftButton.Click += new System.EventHandler(this.UpLeftButton_Click);
-            // 
-            // UpButton
-            // 
-            this.UpButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpButton.Location = new System.Drawing.Point(216, 137);
-            this.UpButton.Name = "UpButton";
-            this.UpButton.Size = new System.Drawing.Size(166, 125);
-            this.UpButton.TabIndex = 1;
-            this.UpButton.UseVisualStyleBackColor = true;
-            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
-            // 
-            // UpRightButton
-            // 
-            this.UpRightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpRightButton.Location = new System.Drawing.Point(396, 137);
-            this.UpRightButton.Name = "UpRightButton";
-            this.UpRightButton.Size = new System.Drawing.Size(166, 125);
-            this.UpRightButton.TabIndex = 2;
-            this.UpRightButton.UseVisualStyleBackColor = true;
-            this.UpRightButton.Click += new System.EventHandler(this.UpRightButton_Click);
-            // 
-            // LeftButton
-            // 
-            this.LeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LeftButton.Location = new System.Drawing.Point(34, 268);
-            this.LeftButton.Name = "LeftButton";
-            this.LeftButton.Size = new System.Drawing.Size(166, 125);
-            this.LeftButton.TabIndex = 3;
-            this.LeftButton.UseVisualStyleBackColor = true;
-            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
-            // 
-            // MidButton
-            // 
-            this.MidButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MidButton.Location = new System.Drawing.Point(216, 268);
-            this.MidButton.Name = "MidButton";
-            this.MidButton.Size = new System.Drawing.Size(166, 125);
-            this.MidButton.TabIndex = 4;
-            this.MidButton.UseVisualStyleBackColor = true;
-            this.MidButton.Click += new System.EventHandler(this.MidButton_Click);
-            // 
-            // RightButton
-            // 
-            this.RightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RightButton.Location = new System.Drawing.Point(396, 268);
-            this.RightButton.Name = "RightButton";
-            this.RightButton.Size = new System.Drawing.Size(166, 125);
-            this.RightButton.TabIndex = 5;
-            this.RightButton.UseVisualStyleBackColor = true;
-            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
-            // 
-            // DownLeftButton
-            // 
-            this.DownLeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DownLeftButton.Location = new System.Drawing.Point(34, 399);
-            this.DownLeftButton.Name = "DownLeftButton";
-            this.DownLeftButton.Size = new System.Drawing.Size(166, 125);
-            this.DownLeftButton.TabIndex = 6;
-            this.DownLeftButton.UseVisualStyleBackColor = true;
-            this.DownLeftButton.Click += new System.EventHandler(this.DownLeftButton_Click);
-            // 
-            // DownButton
-            // 
-            this.DownButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DownButton.Location = new System.Drawing.Point(215, 399);
-            this.DownButton.Name = "DownButton";
-            this.DownButton.Size = new System.Drawing.Size(166, 125);
-            this.DownButton.TabIndex = 7;
-            this.DownButton.UseVisualStyleBackColor = true;
-            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
-            // 
-            // DownRightButton
-            // 
-            this.DownRightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DownRightButton.Location = new System.Drawing.Point(396, 399);
-            this.DownRightButton.Name = "DownRightButton";
-            this.DownRightButton.Size = new System.Drawing.Size(166, 125);
-            this.DownRightButton.TabIndex = 8;
-            this.DownRightButton.UseVisualStyleBackColor = true;
-            this.DownRightButton.Click += new System.EventHandler(this.DownRightButton_Click);
-            // 
             // ResetButton
             // 
             this.ResetButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -162,15 +72,105 @@
             this.ResetButton.UseVisualStyleBackColor = true;
             this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
             // 
-            // Form1
+            // DownRightButton
+            // 
+            this.DownRightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownRightButton.Location = new System.Drawing.Point(396, 399);
+            this.DownRightButton.Name = "DownRightButton";
+            this.DownRightButton.Size = new System.Drawing.Size(166, 125);
+            this.DownRightButton.TabIndex = 8;
+            this.DownRightButton.UseVisualStyleBackColor = true;
+            this.DownRightButton.Click += new System.EventHandler(this.DownRightButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownButton.Location = new System.Drawing.Point(215, 399);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(166, 125);
+            this.DownButton.TabIndex = 7;
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // DownLeftButton
+            // 
+            this.DownLeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownLeftButton.Location = new System.Drawing.Point(34, 399);
+            this.DownLeftButton.Name = "DownLeftButton";
+            this.DownLeftButton.Size = new System.Drawing.Size(166, 125);
+            this.DownLeftButton.TabIndex = 6;
+            this.DownLeftButton.UseVisualStyleBackColor = true;
+            this.DownLeftButton.Click += new System.EventHandler(this.DownLeftButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RightButton.Location = new System.Drawing.Point(396, 268);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(166, 125);
+            this.RightButton.TabIndex = 5;
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // MidButton
+            // 
+            this.MidButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MidButton.Location = new System.Drawing.Point(216, 268);
+            this.MidButton.Name = "MidButton";
+            this.MidButton.Size = new System.Drawing.Size(166, 125);
+            this.MidButton.TabIndex = 4;
+            this.MidButton.UseVisualStyleBackColor = true;
+            this.MidButton.Click += new System.EventHandler(this.MidButton_Click);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeftButton.Location = new System.Drawing.Point(34, 268);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(166, 125);
+            this.LeftButton.TabIndex = 3;
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // UpRightButton
+            // 
+            this.UpRightButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpRightButton.Location = new System.Drawing.Point(396, 137);
+            this.UpRightButton.Name = "UpRightButton";
+            this.UpRightButton.Size = new System.Drawing.Size(166, 125);
+            this.UpRightButton.TabIndex = 2;
+            this.UpRightButton.UseVisualStyleBackColor = true;
+            this.UpRightButton.Click += new System.EventHandler(this.UpRightButton_Click);
+            // 
+            // UpButton
+            // 
+            this.UpButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpButton.Location = new System.Drawing.Point(216, 137);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(166, 125);
+            this.UpButton.TabIndex = 1;
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // UpLeftButton
+            // 
+            this.UpLeftButton.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpLeftButton.Location = new System.Drawing.Point(34, 137);
+            this.UpLeftButton.Name = "UpLeftButton";
+            this.UpLeftButton.Size = new System.Drawing.Size(166, 125);
+            this.UpLeftButton.TabIndex = 0;
+            this.UpLeftButton.UseVisualStyleBackColor = true;
+            this.UpLeftButton.Click += new System.EventHandler(this.UpLeftButton_Click);
+            // 
+            // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 564);
             this.Controls.Add(this.Box);
             this.Location = new System.Drawing.Point(12, 12);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe Application";
             this.Box.ResumeLayout(false);
             this.ResumeLayout(false);
 
--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Tic_Tac_Toe_Application
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
         bool turn = true;
 
@@ -160,7 +160,7 @@ namespace Tic_Tac_Toe_Application
             }
 
         }
-        public Form1()
+        public TicTacToe()
         {
             InitializeComponent();
         }
